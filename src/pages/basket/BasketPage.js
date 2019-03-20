5286b9f3a43a8e584dd5b8ff832e3cf64f312e31
Damian Golojuch <damiangolojuch@gmail.com>
2019-03-20 20:46:13 +0100

    v0.6

--- src/pages/basket/BasketPage.js
+++ src/pages/basket/BasketPage.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 import './BasketPage.css';
 import Title from '../../components/title/Title';
-import SummaryBox from '../../components/summaryBox/SummaryBox';
-import BasketProductList from '../../components/basketProductList/BasketProductList';
-import products from './../../data/products.json';
 
 class BasketPage extends Component {
     render() {
-        const list = products.productList.map((product) => {
-            product.count = 1;
-            return product;
-        });
-
         return (
             <div className="BasketPage">
                 <Title title="Twój koszyk" />
-                <BasketProductList list={list} onRemoveFromBasket={() => alert('Usunięto!')} />
-                <SummaryBox className="BasketPage__SummaryBox" price={0} />
             </div>
         );
     }
