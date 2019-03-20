69d6418e22ed127eafa16fbbb7e58ec110ae2716
Damian Golojuch <damiangolojuch@gmail.com>
2019-03-20 20:47:40 +0100

    v0.5

--- src/pages/product/ProductPage.js
+++ src/pages/product/ProductPage.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
 import './ProductPage.css';
-import Title from '../../components/title/Title';
-import ProductDetails from '../../components/productDetails/ProductDetails';
-import NavigationLink from '../../components/navigationLink/NavigationLink';
-import products from './../../data/products.json';
 
 class ProductPage extends Component {
     render() {
-        const {
-            match: {
-                params: { id },
-            }
-        } = this.props;
-        const product = products.productList.find((product) => product.id === +id);
-
         return (
             <div className="ProductPage">
-                {product && (
-                    <>
-                        <Title title={product.title} />
-                        <ProductDetails
-                            photoUrl={product.photoUrl}
-                            price={product.price}
-                            description={product.description}
-                            rating={product.rating}
-                            onAddToBasket={() => alert('Dodano!')}
-                        />
-                    </>
-                )}
-                <NavigationLink to="/" iconType="left">Wszystkie produkty</NavigationLink>
+                ProductPage
             </div>
         );
     }
